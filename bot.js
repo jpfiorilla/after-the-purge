7d2c0a46bb76b9abae4740130f534be8a2a9f002
audiosprite <jpfiorilla@gmail.com>
2016-12-29 15:15:33 -0500

    adds purge text

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const imdb = require('imdb-api');
 const webshot = require('webshot');
-const request = require('request');
+const request = require('request-promise');
+
+
 
 imdb.getById('tt0496424')
     .then(things => {
@@ -10,87 +12,17 @@ imdb.getById('tt0496424')
 .then(url => {
     request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-        const response = JSON.parse(body)
-        console.log("Got a response: ", response.picture)
-        return response;
-    //   } else {
-    //     console.log("Got an error: ", error, ", status code: ", response.statusCode)
+        const purgetext = " just before the city's annual purge, when all crime is legal.";
+        // const response = JSON.parse(body)
+        // console.log("Got a response: ", typeof(body))
+        let descriptionDiv = 'class="summary_text" itemprop="description"';
+        let preIndex = body.indexOf(descriptionDiv);
+        let searchIndex = preIndex + body.substring(preIndex).indexOf('</div>');
+        const newBody = body.substring(0, searchIndex-14) + purgetext + body.substring(searchIndex);
+        console.log(newBody.substring(preIndex, searchIndex+purgetext.length+200));
+        webshot(newBody, 'image.png', {siteType:'html'}, function(err) {
+            console.log('screenshot now saved to image.png');
+        });
         }
     })
 })
-.then(response => {
-    if (!response) response = '<html><body>Hello World</body></html>';
-    webshot(response, 'google.png', {siteType:'html'}, function(err) {
-        console.log('screenshot now saved to google.png');
-    });
-})
-
-
-
-/*
-example.js
- 
-this electron script will screen-capture the webpage http://electron.atom.io
- 
-instruction
-    1. save this script as example.js
-    2. run the shell command:
-        $ npm install electron-lite && \
-            printf '{"main":"example.js","name":"undefined","version":"0.0.1"}' > \
-            package.json && \
-            ./node_modules/.bin/electron . --disable-overlay-scrollbar --enable-logging
-    3. view screencapture ./screen-capture.testExampleJs.browser.png
-*/
- 
-/*jslint
-    browser: true,
-    maxerr: 8,
-    maxlen: 96,
-    node: true,
-    stupid: true
-*/
-
-// "just before the city's annual purge, when all crime is legal."
-
-/*
-const getPic = function (url) {
-    'use strict';
-    var options, modeNext, onNext;
-    modeNext = 0;
-    onNext = function (data) {
-        modeNext += 1;
-        switch (modeNext) {
-        case 1:
-            // wait for electron to init 
-            require('electron').app.once('ready', onNext);
-            break;
-        case 2:
-            // init options 
-            options = { frame: false, height: 768, width: 1024, x: 0, y: 0 };
-            // init browserWindow; 
-            options.BrowserWindow = require('electron').BrowserWindow;
-            options.browserWindow = new options.BrowserWindow(options);
-            // goto next step when webpage is loaded 
-            options.browserWindow.webContents.once('did-stop-loading', onNext);
-            // open url 
-            // options.browserWindow.loadURL('http://electron.atom.io');
-            options.browserWindow.loadURL(url);
-            break;
-        case 3:
-            // screen-capture webpage 
-            options.browserWindow.capturePage(options, onNext);
-            break;
-        case 4:
-            // save screen-capture 
-            require('fs').writeFileSync(
-                'screen-capture.testExampleJs.browser.png',
-                data.toPng()
-            );
-            // exit 
-            process.exit(0);
-            break;
-        }
-    };
-    onNext();
-};
-*/
